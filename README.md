d9122b5948e5a6c27b3e09085d7f93941cc49f70
Jason Stallings <jason@stallin.gs>
2016-09-17 15:04:07 -0500

    Moved to it's own repo.

---
+++ README.md
@@ -0,0 +1,34 @@
+# Description
+
+This command line app will automatically upload ngrok domains to [ngrokrock.com](https://ngrokrock.com).
+
+## Installation
+
+```
+npm install -g ngrokrock
+```
+
+## Usage
+
+```
+$ ngrokrock --help
+
+  Usage: ngrokrock [options]
+
+  Options:
+
+    -h, --help              output usage information
+    -V, --version           output the version number
+    -u, --username <value>  Your ngrokrock username.
+    -p, --password <value>  Your ngrokrock password.
+    -h, --host [value]      The ngrok API host.
+    -a, --api [value]       The ngrokrock API host.
+```
+
+Example:
+
+```
+ngrokrock -u Jason -p YOUR_API_KEY
+```
+
+Get an API key from [ngrokrock.com](https://ngrokrock.com).
---
+++ src/index.js
@@ -0,0 +1,115 @@
+#!/usr/bin/env node
+const program = require('commander');
+const rp = require('request-promise');
+const _ = require('underscore');
+const pjson = require('../package.json');
+let tunnels;
+
+program
+	.version(pjson.version)
+	.option('-u, --username <value>', 'Your ngrokrock username.')
+	.option('-p, --password <value>', 'Your ngrokrock password.')
+	.option('-h, --host [value]', 'The ngrok API host.', 'http://127.0.0.1:4040')
+	.option('-a, --api [value]', 'The ngrokrock API host.', 'https://ngrokrock.com')
+	.parse(process.argv);
+
+if (!program.username) {
+	console.log('--username option required')
+	process.exit();
+}
+
+if (!program.password) {
+	console.log('--password option required')
+	process.exit();
+}
+
+const host = program.host;
+
+check();
+
+function buildDomain(query) {
+	let parse = program.api.split('://');
+	let domain = parse[1] + '/api/v1/domains';
+	let full = parse[0] + '://' + program.username + ':' + program.password + '@' + domain;
+
+	if (query) {
+		full += query;
+	}
+	return full;
+}
+
+function check() {
+	rp(program.host + '/api/tunnels')
+		.then(parseJson)
+		.then(parseTunnels)
+		.then(_.uniq)
+		.then(uniqueTunnels)
+		.then((response) => {
+			if ( !_.isEqual(response, tunnels) ) {
+				tunnels = response;
+				if (!_.isEmpty(tunnels)) {
+					submit(response);
+				}
+			}
+		})
+		.catch((err) => {
+			// ngrok isn't running.
+		}).finally(() => {
+			defer();
+		});
+}
+function submit(tunnels) {
+	let domain = buildDomain(buildQuery(tunnels));
+	let options = {
+		method: 'POST',
+		uri: domain,
+	};
+
+	rp(options)
+		.then(parseJson)
+		.then((response) => {
+			if (response.success) {
+				console.log(tunnels + ' added!');
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+}
+
+function uniqueTunnels(tunnels) {
+	let promise = new Promise((resolve, reject) => {
+		rp(buildDomain())
+			.then(parseJson)
+			.then((response) => {
+				resolve(_.difference(tunnels, response));
+			});
+	});
+
+	return promise;
+}
+
+function defer() {
+	setTimeout(() => {
+		check();
+	}, 5000);
+}
+
+function buildQuery(tunnels) {
+	let query = '?';
+	for (let domain of tunnels) {
+		if (query !== '?') query += '&';
+		query += 'domain[]=' + encodeURIComponent(domain);
+	}
+	return query;
+}
+
+function parseJson(response){
+	return JSON.parse(response);
+}
+
+function parseTunnels(response) {
+	return response.tunnels.map((tunnel) => {
+		return tunnel.public_url.replace(/^.*?:\/\//, '');
+	});
+}
